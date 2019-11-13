5c2154f20228103525e3a5464bc4d8a4be6322b2
Bertrand Benoit <contact@bertrand-benoit.net>
2019-11-13 15:06:25 +0100

    Improved/fixed the way resource group is automatically found/defined from Website name.

--- scripts/stopContinuousJob.sh
+++ scripts/stopContinuousJob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Author: Bertrand BENOIT <bertrand@bertrand-benoit.net>
-# Version: 2.0
+# Version: 2.1
 #
 # Description: disable automagically all continuous Job of specified website.
 #
@@ -9,9 +9,7 @@
 
 [ $# -lt 1 ] && echo -e "Usage: $0 <website>" >&2 && exit 1
 
-# N.B.: resource group may be named differently, and thus it would not work.
 website="$1"
-resourceGroup="itclients-${website//itc-}-prod-rg"
 
 function oldCliV1() {
   echo -e "If error occurs, you may need to run the following instructions:\n\tazure login"
@@ -23,17 +21,32 @@ function oldCliV1() {
   done
 }
 
+function defineResourceGroup() {
+  local _website="$1"
+
+  # Old way to define resource group ..
+  # resourceGroupAlt="${website}-prod-rg"
+  # resourceGroup="itclients-${website//itc-}-prod-rg"
+  local namePart=$( echo "$_website" |sed -e 's/^itcp*-//')
+  local resourceGroup=$( az group list |jq '.[].name' -r |uniq |grep -i "$namePart" )
+
+  echo "Defined resource group for Website '$_website': '$resourceGroup'" >&2
+  echo "$resourceGroup"
+}
+
 # usage: newCliV2 <website> <resource group>
 function newCliV2() {
     local _website="$1" _resourceGroup="$2"
 
-    echo -e "If error occurs, you may need to run the following instructions:\n\taz login"
-
     echo -e "Loading continuous job list for website '$_website', in resource-group ''$_resourceGroup' ..."
     for continuousJob in $( az webapp webjob continuous list --name "$_website" --resource-group "$_resourceGroup" |jq '.[].name' -r |sed -e "s/$_website//"  ); do
       echo -e "Stopping continuous job $continuousJob ... "
-      ! az webapp webjob continuous stop --name "$_website" --resource-group "$_resourceGroup" --webjob-name "$continuousJob" && echo "FAILED" >&2 && exit 2
+      ! az webapp webjob continuous stop --name "$_website" --resource-group "$_resourceGroup" --webjob-name "$continuousJob" && echo "FAILED" >&2 && return 0
     done
+
+    return 128
 }
 
+echo -e "If error occurs, you may need to run the following instructions:\n\taz login"
+resourceGroup=$( defineResourceGroup "$website")
 newCliV2 "$website" "$resourceGroup"
