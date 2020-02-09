d6607c24b922e5c87e69f3a1c71587a8d0235c8d
Bertrand Benoit <contact@bertrand-benoit.net>
2020-02-09 12:27:07 +0100

    Migrated to scripts-common. Updated various message, and way to do things.

--- stopContinuousJob.sh
+++ stopContinuousJob.sh
@@ -1,50 +1,70 @@
 #!/bin/bash
 #
-# Author: Bertrand BENOIT <bertrand@bertrand-benoit.net>
-# Version: 2.1
+# Author: Bertrand BENOIT <contact@bertrand-benoit.net>
+# Version: 2.2
 #
-# Description: disable automagically all continuous Job of specified website.
-#
-# Sample: stopContinuousJob.sh itc-cegid
+# Description: stops automatically all continuous Jobs of specified Azure website.
 
-[ $# -lt 1 ] && echo -e "Usage: $0 <website>" >&2 && exit 1
+#####################################################
+#                General configuration.
+#####################################################
+export CATEGORY="stopContinuousJob"
 
+currentDir=$( dirname "$( command -v "$0" )" )
+export GLOBAL_CONFIG_FILE="$currentDir/default.conf"
+export CONFIG_FILE="${HOME:-/home/$( whoami )}/.config/stopContinuousJob.conf"
+
+# shellcheck disable=1090
+[ -n "$SCRIPTS_COMMON_PATH" ] && . "$SCRIPTS_COMMON_PATH"
+
+checkBin jq || errorMessage "This tool requires jq. Install it please, and then run this tool again."
+checkBin az || errorMessage "This tool requires az (Azure CLI v2). Install it please, and then run this tool again."
+
+checkAndSetConfig "patterns.removeMatchingParts" "$CONFIG_TYPE_OPTION"
+REMOVE_NAME_MATCHING_PARTS="$LAST_READ_CONFIG"
+
+
+#####################################################
+#                Command line management.
+#####################################################
+[ $# -lt 1 ] && errorMessage "Usage: $0 <website>"
 website="$1"
 
+#####################################################
+#                Functions.
+#####################################################
+# Usage: oldCliV1 <website>
+# Previous version with Azure CLI v1, for information.
 function oldCliV1() {
-  echo -e "If error occurs, you may need to run the following instructions:\n\tazure login"
+  writeMessage "If error occurs, you may need to run the following instructions:\n\tazure login"
 
   for continuousJob in $( azure site job list $website |grep continuous |awk '{print $2}' ); do
-    echo -ne "Stopping continuous job $continuousJob ... "
-    ! azure site job stop $continuousJob $website && echo "FAILED" >&2 && exit 2
+    writeMessageSL "Stopping continuous job $continuousJob ... "
+    ! azure site job stop $continuousJob $website && echo "FAILED" >&2 && continue
     echo "OK"
   done
 }
 
 function defineResourceGroup() {
-  local _website="$1"
+  local _website="$1" namePart resourceGroup
 
-  # Old way to define resource group ..
-  # resourceGroupAlt="${website}-prod-rg"
-  # resourceGroup="itclients-${website//itc-}-prod-rg"
-  local namePart=$( echo "$_website" |sed -e 's/^itcp*-//')
-  local resourceGroup=$( az group list |jq '.[].name' -r |uniq |grep -i "$namePart" )
+  namePart=$( echo "$_website" |sed -e 's/^itcp*-//')
+  resourceGroup=$( az group list |jq '.[].name' -r |uniq |grep -i "$namePart" )
+  [ -z "$resourceGroup" ] && return 0
 
-  echo "Defined resource group for Website '$_website': '$resourceGroup'" >&2
   echo "$resourceGroup"
+  return 0
 }
 
-# usage: newCliV2 <website> <resource group>
+# Usage: newCliV2 <website> <resource group>
 function newCliV2() {
     local _website="$1" _resourceGroup="$2"
 
-    echo -e "Loading continuous job list for website '$_website', in resource-group ''$_resourceGroup' ..."
+    writeMessage "Loading continuous job list for website '$_website', in resource-group ''$_resourceGroup' ..."
     for continuousJob in $( az webapp webjob continuous list --name "$_website" --resource-group "$_resourceGroup" |jq '.[].name' -r |sed -e "s/$_website//"  ); do
-      echo -e "Stopping continuous job $continuousJob ... "
-      ! az webapp webjob continuous stop --name "$_website" --resource-group "$_resourceGroup" --webjob-name "$continuousJob" && echo "FAILED" >&2 && return 0
+      writeMessage "Stopping continuous job $continuousJob ... "
+      ! az webapp webjob continuous stop --name "$_website" --resource-group "$_resourceGroup" --webjob-name "$continuousJob" && warning "Failed to stop continuous job $continuousJob."
     done
-
-    return 128
 }
 
 echo -e "If error occurs, you may need to run the following instructions:\n\taz login"
